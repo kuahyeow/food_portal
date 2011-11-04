d12e6695f91f0d56f3f543c9cca535cba959974b
kuahyeow <kuahyeow@gmail.com>
2011-11-05 09:47:16 +1300

    First mobile page!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def tactile_image(name)
+    if name =~ /beer/i
+      image_tag('beer.jpg')
+    elsif name =~ /fish/i
+      image_tag('salmon.jpg')
+    elsif name =~ /vege/i
+      image_tag('tomato.jpg')
+    else
+      image_tag('beer.jpg')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FoodPortal::Application.routes.draw do
+
+  root :to => 'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
