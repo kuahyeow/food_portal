0fc63554f48b5584c7b786be16102d63aed5046e
kuahyeow <kuahyeow@gmail.com>
2011-11-04 22:09:36 +1300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodPortal::Application.config.secret_token = 'a07b86ee43592395b00b89024d9713d6dc22534f366047965e7200401878a8c9f78769c813fcf1d926b9801975c57d33beb62cfef0165afad4bc4dece76570c2'
